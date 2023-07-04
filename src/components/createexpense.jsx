ac18797102c84dabf164a7f6863b5e0fd056a636
SirraRakesh <sirrarakesh2000@gmail.com>
2023-07-04 22:17:08 +0530

    just check

--- src/components/createexpense.jsx
+++ src/components/createexpense.jsx
@@ -14,6 +14,7 @@ function Createexpense() {
   const [cost, setCost] = useState(0);
   const [name, setName] = useState("");
   const [play, { stop }] = useSound(notification);
+  // Its just checking
 
   const [data, setData] = useData();
   const date = new Date();
